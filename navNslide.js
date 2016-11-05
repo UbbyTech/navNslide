1fc04e0b7ac158ecd6ebb62c430b818dcaf490da
Joseph P Bourne <kiratsuchi@live.com>
2016-11-05 05:16:04 -0700

    small change

--- navNslide.js
+++ navNslide.js
@@ -1,8 +1,27 @@
 function change()
 {
-	var background = $(".slider").css("background", "#ccc");
-	var margin = $(".slider ul").css("listStyle", "none");
-		
+	var hidecb = $("#openNav").hide();
+	if (hidecb.checked())
+	{
+		$("body").css("background", "blue");
+	}
+
+
+	var background = $(".slider").css("background", "#ccc")
+	.css("width", "240");
+	var height = $(".slider").css("height", "100%");
+
+
+}
+
+function resetNav()
+{
+	var reset = $(".slider, .slider ul, .slider ul li, .slider ul li a")
+		.css("display", "block")
+		.css("margin", "0")
+		.css("padding", "0")
+		.css("border", "none")
+		.css("outline", "none");
 }
 
 
@@ -11,7 +30,19 @@ $(document).ready(function()
 {
 	if($("nav").hasClass("slider"))
 	{
+		resetNav();
 		change();
+
+	}
+
+	else if($("nav").hasClass("sliderTop"))
+	{
+
+	}
+
+	else
+	{
+
 	}
 });
 
