4f1f4fe47a07518a089c16865a7d25e52f349570
Joseph P Bourne <kiratsuchi@live.com>
2016-11-09 23:42:50 -0800

    added openNav func to open nav

--- navNslide.js
+++ navNslide.js
@@ -4,10 +4,9 @@ function slider()
 	var background = $(".slider").css("background", "#ccc")
 	.css("width", "240");
 	var height = $(".slider").css("height", "100%");
-	var height = $(".slider").css("width", "240");
 	var pos1 = $(".slider").css("position", "absolute");
 	var pos2 = $(".slider").css("left", "-240");
-	
+
 
 }
 
@@ -22,6 +21,17 @@ function btmSlider()
 
 }
 
+function openNav()
+{
+	var pos1 = $(".slider").css("position", "absolute");
+	var pos2 = $(".slider").css("left", "0");
+}
+
+function closeNav()
+{
+
+}
+
 function resetNav()
 {
 	var reset = $(".slider, .slider ul, .slider ul li, .slider ul li a")
@@ -29,7 +39,9 @@ function resetNav()
 		.css("margin", "0")
 		.css("padding", "0")
 		.css("border", "none")
-		.css("outline", "none");
+		.css("outline", "none")
+		.css("font-family", "sans-serif")
+		.css("color", "black");
 }
 
 
